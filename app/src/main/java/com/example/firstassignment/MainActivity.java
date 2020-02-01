ac81ff80c75be6dc06b858aba9a19b2b78163e3d
PascalCoder <pbaconqueror7@yahoo.com>
2020-02-01 14:37:10 -0500

    added login functionality

--- app/src/main/java/com/example/firstassignment/MainActivity.java
+++ app/src/main/java/com/example/firstassignment/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.firstassignment;
 
 import android.content.Intent;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -13,14 +15,24 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.btn_create_acct)
     Button btnCreateAcct;
 
+    @BindView(R.id.btn_sign_in)
+    Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        btnLogin.setOnClickListener(view -> navigateToLoginPage());
+
         btnCreateAcct.setOnClickListener(view -> createAcct());
 
+    }
+
+    private void navigateToLoginPage() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
     private void createAcct() {
